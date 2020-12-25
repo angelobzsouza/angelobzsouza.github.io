2d833e8c3e5f3f2267bda2044369bf20fbf5c0b1
Angelo Souza <angelo.bz.souza@gmail.com>
2020-12-24 22:39:04 -0300

    Change to video

--- script.js
+++ script.js
@@ -1,45 +1,17 @@
-const contagem = document.getElementById("contagem")
-const qavtb = document.getElementById("quando-a-vontade-bater")
 const redeGlobo = document.getElementById("rede-globo")
-const newYearDate = new Date('2021-01-01 00:00:00')
+const video = document.getElementById("video")
 const button = document.getElementById("start")
+const body = document.getElementById("body")
+const contagem = document.getElementById("contagem")
+
+const newYearDate = new Date('2020-12-25 00:00:00')
 let stopRedeGlobo = false;
 
 window.addEventListener('load', () => {
-    const qavtbPlayling = false;
-    contagemPlayling = false
     startCountdown()
     playLoopRedeGlobo();
-    startMusiceXSecondsBefore(contagem, 13, contagemPlayling);
-    startMusiceXSecondsBefore(qavtb, 45, qavtbPlayling);
+    startVideoXSecondsBefore(video, 311, playing = false);
 })
-
-const zeroPad = (num, places) => String(num).padStart(places, '0')
-
-const startCountdown = () => {
-    const h1 = document.getElementById("countdown")
-    setInterval(() => {
-        let [days, hours, minutes, seconds] = calculateTimeDifferenceFormatted(newYearDate, new Date())
-        h1.innerHTML = `Faltam ${days} dias e ${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${zeroPad(seconds, 2)} horas`
-    }, 100);
-}
-
-const startMusiceXSecondsBefore = (music, seconds, playing) => {
-    setInterval(() => {
-        if (calculateTimeDifference(newYearDate, new Date()) < seconds && !playing) {
-            playing = true;
-            stopRedeGlobo = true;
-            redeGlobo.pause();
-            music.play();
-        }
-    }, 1)
-}
-
-const playLoopRedeGlobo = () => {
-    setInterval(() => {
-        if (!isPlaying(redeGlobo) && !stopRedeGlobo) redeGlobo.play()
-    }, 1)
-}
 
 const calculateTimeDifference = (endDate, startDate) => {
     return (endDate.getTime() - startDate.getTime()) / 1000
@@ -54,6 +26,35 @@ const calculateTimeDifferenceFormatted = (endDate, startDate) => {
     return [days, hours, minutes, seconds]
 }
 
+const zeroPad = (num, places) => String(num).padStart(places, '0')
+
+const startCountdown = () => {
+    const h1 = document.getElementById("countdown")
+    setInterval(() => {
+        let [days, hours, minutes, seconds] = calculateTimeDifferenceFormatted(newYearDate, new Date())
+        h1.innerHTML = `Faltam ${days} dias e ${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${zeroPad(seconds, 2)} horas`
+    }, 100);
+}
+
 const isPlaying = (audio) => {
     return (audio.duration > 0 && !audio.paused) ? true : false; 
+}
+
+const playLoopRedeGlobo = () => {
+    setInterval(() => {
+        if (!isPlaying(redeGlobo) && !stopRedeGlobo) redeGlobo.play()
+    }, 1)
+}
+
+const startVideoXSecondsBefore = (video, seconds, playing) => {
+    setInterval(() => {
+        if (calculateTimeDifference(newYearDate, new Date()) < seconds && !playing) {
+            video.style.display = "block";
+            contagem.style.display = "none";
+            playing = true;
+            stopRedeGlobo = true;
+            redeGlobo.pause();
+            video.play();
+        }
+    }, 1)
 }
